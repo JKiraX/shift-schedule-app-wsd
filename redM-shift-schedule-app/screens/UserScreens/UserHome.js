47d6e7f525edb36b1aef11a9aa8d06d66e8245ee
Juhi Kira <106545129+JKiraX@users.noreply.github.com>
2024-05-07 09:36:54 +0200

    style changes

--- redM-shift-schedule-app/screens/UserScreens/UserHome.js
+++ redM-shift-schedule-app/screens/UserScreens/UserHome.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import ShiftCard from '../../components/Cards/ShiftCard';
-
+ 
 const UserHomeScreen = ({ navigation }) => {
   const [shiftData, setShiftData] = useState([]);
-
+ 
   useEffect(() => {
     // Fetch shift data from the database
     fetchShiftData();
   }, []);
-
+ 
   const fetchShiftData = async () => {
     try {
       // Replace this with your actual database query logic
@@ -21,23 +21,36 @@ const UserHomeScreen = ({ navigation }) => {
           assignedUsers: ['User 1', 'User 2'],
         },
         {
+          shiftName: '8am Shift',
+          startTime: '08:00',
+          endTime: '16:00',
+          assignedUsers: ['User 3', 'User 4'],
+        },
+        {
           shiftName: '2pm Shift',
           startTime: '14:00',
           endTime: '22:00',
-          assignedUsers: ['User 3', 'User 4'],
+          assignedUsers: ['User 1', 'User 2'],
+        },
+ 
+        {
+          shiftName: '10pm Shift',
+          startTime: '22:00',
+          endTime: '06:00',
+          assignedUsers: ['User 1', 'User 2'],
         },
         // Add more shift data objects as needed
       ];
-
+ 
       setShiftData(data);
     } catch (error) {
       console.error('Error fetching shift data:', error);
     }
   };
-
+ 
   return (
     <View style={styles.container}>
-    
+   
       {shiftData.map((shift, index) => (
         <ShiftCard
           key={index}
@@ -50,14 +63,14 @@ const UserHomeScreen = ({ navigation }) => {
     </View>
   );
 };
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
+    justifyContent: 'left',
     padding: 20,
   },
 });
-
+ 
 export default UserHomeScreen;
