33e1b684dcf6f95578346fb1c100749aa0f7be23
Muzzammil <muzzammilk617@gmail.com>
2024-05-07 10:53:38 +0200

    Merge branch 'main' into muzzammil-Home

--- redM-shift-schedule-app/screens/UserScreens/UserHome.js
+++ redM-shift-schedule-app/screens/UserScreens/UserHome.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { SafeAreaView } from 'react-native';
-import CalendarStrip from 'react-native-calendar-strip';
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native";
+import CalendarStrip from "react-native-calendar-strip";
 
 export default function UserHomeScreen({ navigation }) {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -14,15 +14,21 @@ export default function UserHomeScreen({ navigation }) {
       <CalendarStrip
         scrollable
         style={{ height: 120, paddingTop: 20, paddingBottom: 10 }}
-        calendarHeaderStyle={{ color: '#9098B1', fontSize: 18, fontWeight: "bold" }}
-        calendarColor={'white'}
-        dateNumberStyle={{ color: '#98C1D9', fontSize:20, fontWeight:"normal" }}
-        dateNameStyle={{ color: '#98C1D9', fontSize: 12, marginTop:5}}
+        calendarHeaderStyle={{
+          color: "#9098B1",
+          fontSize: 18,
+          fontWeight: "bold",
+        }}
+        calendarColor={"white"}
+        dateNumberStyle={{
+          color: "#98C1D9",
+          fontSize: 20,
+          fontWeight: "normal",
+        }}
+        dateNameStyle={{ color: "#98C1D9", fontSize: 12, marginTop: 5 }}
         iconContainer={{ flex: 0.1 }}
         selectedDate={selectedDate}
         onDateSelected={onDateSelected}
-        
-        
       />
     </SafeAreaView>
   );
