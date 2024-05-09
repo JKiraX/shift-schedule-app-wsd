36bfafc0e68901481160ff9e79b3c9e0e767f3fc
lizziesekatiredmps <lizzie.sekati@redmps.com>
2024-05-09 09:58:32 +0200

    Loginscreen

--- redM-shift-schedule-app/App.js
+++ redM-shift-schedule-app/App.js
@@ -2,13 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import UserNav from './components/Navigation/UserNav';
-import AdminNav from './components/Navigation/AdminNav';
-
+// import AdminNav from './components/Navigation/AdminNav';
+ 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <AdminNav/>
+      <UserNav/>
     </NavigationContainer>
   );
 }
