a48590da69e112188190e47a9e8bae126be611a2
Muzzammil <muzzammilk617@gmail.com>
2024-05-15 12:13:08 +0200

    drop down as a component

--- redM-shift-schedule-app/screens/UserScreens/UserSchedule.js
+++ redM-shift-schedule-app/screens/UserScreens/UserSchedule.js
@@ -1,46 +1,24 @@
 import * as React from "react";
 import { Text, View } from "react-native";
-import { SelectList } from "react-native-dropdown-select-list";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { MaterialIcons } from "@expo/vector-icons";
+import DropdownComponent from "../../components/Dropdown/dropdownComponent";
 
 export default function UserScheduleScreen({ navigation }) {
-  const [selected, setSelected] = React.useState("");
   const data = [
     { key: "1", value: "Yusheen" },
-    { key: "2", value: "Roxxane" },
+    { key: "2", value: "Roxanne" },
     { key: "3", value: "Hope" },
     { key: "4", value: "Mpho" },
     { key: "5", value: "Charlotte" },
   ];
+
+  const handleSelect = (selected) => {
+    console.log(selected);
+  }
+
   return (
     <SafeAreaProvider style={{ flex: 1, alignItems: "center", marginTop: 20 }}>
-      <SelectList
-        setSelected={(val) => setSelected(val)}
-        data={data}
-        save="value"
-        onSelect={() => console.log(selected)}
-        //styling
-        placeholder={
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <MaterialIcons name="person-outline" size={24} color="black" />
-            <Text style={{ marginLeft: 10 }}>Select a user</Text>
-          </View>
-        }
-        notFoundText="User not found"
-        boxStyles={{
-          maxWidth: 350,
-          minWidth: 350,
-          backgroundColor: "white",
-          borderColor: "#3D5A80",
-        }}
-        dropdownStyles={{
-          maxWidth: 350,
-          minWidth: 350,
-          backgroundColor: "white",
-          borderColor: "#3D5A80",
-        }}
-      />
+      <DropdownComponent data={data} onSelect={handleSelect} />
     </SafeAreaProvider>
   );
 }
