bc9b06cac5046a53f19d6951ba6617449bfe3736
lizziesekatiredmps <lizzie.sekati@redmps.com>
2024-05-30 13:55:07 +0200

    "Replaced AdminNav with UserNav in App.js and removed unnecessary imports"

--- redM-shift-schedule-app/App.js
+++ redM-shift-schedule-app/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-<<<<<<< HEAD
-import UserNav from './components/Navigation/UserNav';
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import UserNav from "./components/Navigation/UserNav";
 //import AdminNav from './components/Navigation/AdminNav';
  
-=======
-//import UserNav from './components/Navigation/UserNav';
-import AdminNav from './components/Navigation/AdminNav';
-
->>>>>>> 2ed4825b50043cadcabd54d9328802cc8c14b371
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <AdminNav/>
+      <UserNav />
     </NavigationContainer>
   );
 }
