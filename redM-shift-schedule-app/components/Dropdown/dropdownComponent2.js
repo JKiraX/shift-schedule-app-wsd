228f3c8878a58f6c27f231f82768f3352f92d044
Juhi Kira <106545129+JKiraX@users.noreply.github.com>
2024-06-14 10:33:02 +0200

    dropdowncomponent.js pulls users from db, dropdowncomponent2 shows leave types for report page

--- redM-shift-schedule-app/components/Dropdown/dropdownComponent2.js
+++ redM-shift-schedule-app/components/Dropdown/dropdownComponent2.js
@@ -3,21 +3,24 @@ import { Text, View } from "react-native";
 import { SelectList } from "react-native-dropdown-select-list";
 import { MaterialIcons } from "@expo/vector-icons";
 
-const DropdownComponent = ({ data = [], onSelect }) => {
+const DropdownComponent2 = ({ data, onSelect }) => {
   const [selected, setSelected] = React.useState("");
+
+  const handleSelect = (val) => {
+    setSelected(val);
+    const selectedItem = data.find(item => item.value === val);
+    onSelect(selectedItem);
+  };
 
   return (
     <SelectList
-      setSelected={(val) => {
-        setSelected(val);
-        onSelect(val);
-      }}
+      setSelected={handleSelect}
       data={data}
       save="value"
       placeholder={
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <MaterialIcons name="person-outline" size={24} color="black" />
-          <Text style={{ marginLeft: 10 }}>Select User</Text>
+          <Text style={{ marginLeft: 10 }}>Select Leave Type</Text>
         </View>
       }
       boxStyles={{
@@ -40,4 +43,4 @@ const DropdownComponent = ({ data = [], onSelect }) => {
   );
 };
 
-export default DropdownComponent;
+export default DropdownComponent2;
